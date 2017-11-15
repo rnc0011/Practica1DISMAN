21083a236553c22b0fc2108c520fef0e2fa3884c
rnc0011 <rnc0011@alu.ubu.es>
2017-11-15 20:56:18 +0100

    Constructor y métodos de Producto y ListaCompra.
    Se han añadido los contructores de las clases y los métodos que permiten
    añadir, eliminar y marcar como comprados productos de la lista.

--- Practica1DISMAN/src/modelo/ListaCompra.java
+++ Practica1DISMAN/src/modelo/ListaCompra.java
@@ -1,5 +1,58 @@
 package modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Clase ListaCompra. Creamos la estructura de la lista de la compra.
+ * 
+ * @author Raúl Negro Carpintero
+ * @author Mario Núñez Izquierdo
+ * @version 1.0
+ *
+ */
 public class ListaCompra {
+	
+	private List<Producto> lista;
+	
+	/**
+	 * Constructor de la clase.
+	 */
+	public ListaCompra() {
+		this.lista = new ArrayList<Producto>();
+	}
+	
+	/**
+	 * Método añadir. Añade productos a la lista de la compra.
+	 * 
+	 * @param producto
+	 */
+	private void añadir(Producto producto) {
+		this.lista.add(producto);
+	}
+	
+	/**
+	 * Método eliminar. Elimina productos de la lista de la compra.
+	 * 
+	 * @param producto
+	 */
+	private void eliminar(Producto producto) {
+		if(lista.size() != 0) {
+			this.lista.remove(producto);
+		}
+	}
+	
+	private void marcarComprado(Producto producto) {
+		this.lista.get(this.lista.indexOf(producto)).productoComprado();
+	}
+	
+	/**
+	 * Método marcarComprado. Utilizado para marcar un producto como comprado.
+	 * 
+	 * @param indice
+	 */
+	private void marcarComprado(int indice) {
+		this.lista.get(indice).productoComprado();
+	}
 
 }
--- Practica1DISMAN/src/modelo/Producto.java
+++ Practica1DISMAN/src/modelo/Producto.java
@@ -1,5 +1,40 @@
 package modelo;
 
+/**
+ * Clase Producto. Creamos la estructura de un producto.
+ * 
+ * @author Raúl Negro Carpintero
+ * @author Mario Núñez Izquierdo
+ * @version 1.0
+ *
+ */
 public class Producto {
-
+	
+	private String nombre;
+	private int cantidad;
+	private float precio;
+	private boolean comprado;
+	
+	/**
+	 * Constructor de la clase.
+	 * 
+	 * @param nombre
+	 * @param cantidad
+	 * @param precio
+	 */
+	public Producto(String nombre, int cantidad, float precio) {
+		this.nombre = nombre;
+		this.cantidad = cantidad;
+		this.precio = precio;
+		this.comprado = false;
+	}
+	
+	/**
+	 * Método productoComprado. Utilizado para marcar un producto como comprado.
+	 * 
+	 */
+	protected void productoComprado() {
+		this.comprado = true;
+	}
+	
 }
