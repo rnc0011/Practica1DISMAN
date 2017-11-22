3b3771dbbe1ed7433ee97dfc3d3572dd5f8d539b
Mario <Mario@10.160.46.197>
2017-11-22 20:58:40 +0100

    Añadida la clase Gestion con el main del proyecto.

---
+++ Practica1DISMAN/src/gestor/Gestion.java
@@ -0,0 +1,26 @@
+package gestor;
+
+public class Gestion {
+
+	public static void main(String[] args) {
+		
+
+	}
+	
+	protected void anadirProducto(String nombre, int cantidad, float precio) {
+		
+	}
+	
+	protected void eliminarProducto(String nombre) {
+		
+	}
+	
+	protected void modificarCantidad(String nombreProducto, int cantidad) {
+		
+	}
+	
+	protected void marcarComprado(String nombre) {
+		
+	}
+
+}
